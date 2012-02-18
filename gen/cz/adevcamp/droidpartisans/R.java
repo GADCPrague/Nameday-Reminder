2a8f2aa206e0859a597256fbcc6f04dc19544077
jirka git <jirkav3@gmail.com>
2012-02-18 16:28:50 +0100

    biny

--- gen/cz/adevcamp/droidpartisans/R.java
+++ gen/cz/adevcamp/droidpartisans/R.java
@@ -14,13 +14,23 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int button=0x7f050000;
+        public static final int button=0x7f060000;
+        public static final int iv_iconCall=0x7f060003;
+        public static final int iv_iconMess=0x7f060004;
+        public static final int tv_contactName=0x7f060002;
+        public static final int tv_date=0x7f060001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int rowitem=0x7f030001;
+    }
+    public static final class raw {
+        public static final int namedays_cz=0x7f040000;
+        public static final int namedays_cz_rev=0x7f040001;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f050001;
+        public static final int hello=0x7f050000;
+        public static final int iv_icon=0x7f050002;
     }
 }
