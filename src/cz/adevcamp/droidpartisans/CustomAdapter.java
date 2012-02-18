071a1f9bb588a5fec83dab5a11727de938498136
jirka git <jirkav3@gmail.com>
2012-02-18 19:16:08 +0100

    CustomAdapter.java

--- src/cz/adevcamp/droidpartisans/CustomAdapter.java
+++ src/cz/adevcamp/droidpartisans/CustomAdapter.java
@@ -7,7 +7,6 @@ import android.app.Activity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AbsListView;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
