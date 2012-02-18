63b09c59d2102f4c92c12afcad97a795ad0f71a9
jirka git <jirkav3@gmail.com>
2012-02-18 19:04:37 +0100

    CustomAdapter.java

--- src/cz/adevcamp/droidpartisans/CustomAdapter.java
+++ src/cz/adevcamp/droidpartisans/CustomAdapter.java
@@ -7,6 +7,7 @@ import android.app.Activity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -19,11 +20,18 @@ public class CustomAdapter extends BaseExpandableListAdapter{
 	Vector<NamedayCsvLoader.Day> vDay;
 	LayoutInflater lIinflater;
 	View v;
+	String[][] children;
 	
 	CustomAdapter(Activity context, List<Contact> lContacts, Vector<NamedayCsvLoader.Day> vDay){
 		this.mContext=context;
 		this.lContacts=lContacts;
 		this.vDay=vDay;
+		children =new String[][] {
+                { "Arnold", "Barry", "Chuck", "David" },
+                { "Ace", "Bandit", "Cha-Cha", "Deuce" },
+                { "Fluffy", "Snuggles" },
+                { "Goldy", "Bubbles" }
+        };
 		lIinflater=mContext.getLayoutInflater();
 		
 	}
@@ -41,31 +49,9 @@ public class CustomAdapter extends BaseExpandableListAdapter{
 		return vDay.get(position).hashCode();
 	}
 	
-	public View getView(final int position, View convertView, ViewGroup parent) {
-		v=convertView;
-		if(v==null)
-			v=lIinflater.inflate(R.layout.rowitem, null);
-		
-		TextView tv_date=(TextView) v.findViewById(R.id.tv_date);
-			TextView tv_name=(TextView) v.findViewById(R.id.tv_contactName);
-			ImageView iv_iconCall=(ImageView) v.findViewById(R.id.iv_iconCall);
-			ImageView iv_iconMess=(ImageView) v.findViewById(R.id.iv_iconMess);
-			
-			
-		
-		tv_date.setText(vDay.get(position).date);
-		
-		//tv_name.setText(((Contact)lContacts.get(position)).getName());
-		tv_name.setText(vDay.get(position).dateName);
-		iv_iconCall.setImageResource(R.drawable.ic_launcher);
-		iv_iconMess.setImageResource(R.drawable.ic_launcher);
-	
-		return v;
-	}
 
 	public Object getChild(int groupPosition, int childPosition) {
-		// TODO Auto-generated method stub
-		return null;
+		return children[0][0];
 	}
 
 	public long getChildId(int groupPosition, int childPosition) {
@@ -81,7 +67,8 @@ public class CustomAdapter extends BaseExpandableListAdapter{
 
 	public int getChildrenCount(int groupPosition) {
 		// TODO Auto-generated method stub
-		return vDay.get(groupPosition).contacts.size();
+		return 0;
+//		return vDay.get(groupPosition).contacts.size();
 	}
 
 	public Object getGroup(int groupPosition) {
@@ -100,8 +87,25 @@ public class CustomAdapter extends BaseExpandableListAdapter{
 
 	public View getGroupView(int groupPosition, boolean isExpanded,
 			View convertView, ViewGroup parent) {
-		// TODO Auto-generated method stub
-		return null;
+		v=convertView;
+		if(v==null)
+			v=lIinflater.inflate(R.layout.rowitem, null);
+		
+		TextView tv_date=(TextView) v.findViewById(R.id.tv_date);
+			TextView tv_name=(TextView) v.findViewById(R.id.tv_contactName);
+			ImageView iv_iconCall=(ImageView) v.findViewById(R.id.iv_iconCall);
+			ImageView iv_iconMess=(ImageView) v.findViewById(R.id.iv_iconMess);
+			
+			
+		
+		tv_date.setText(vDay.get(groupPosition).date);
+		
+		//tv_name.setText(((Contact)lContacts.get(position)).getName());
+		tv_name.setText(vDay.get(groupPosition).dateName);
+		iv_iconCall.setImageResource(R.drawable.ic_launcher);
+		iv_iconMess.setImageResource(R.drawable.ic_launcher);
+	
+		return v;
 	}
 
 	public boolean hasStableIds() {
