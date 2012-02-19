5ef0ffaf74e4dd4e84a592fbb2b4473b51ac52ae
Eugen Krizo <eugen.krizo@gmail.com>
2012-02-19 12:52:23 +0100

    cleanup

--- src/cz/adevcamp/droidpartisans/CustomAdapter.java
+++ src/cz/adevcamp/droidpartisans/CustomAdapter.java
@@ -17,31 +17,25 @@ public class CustomAdapter extends BaseExpandableListAdapter {
 	List<Contact> lContacts;
 	Vector<NamedayCsvLoader.Day> vDay;
 	LayoutInflater lIinflater;
-	View v;
-	String[][] children;
 
 	CustomAdapter(ExpandableListActivity context, List<Contact> lContacts,
 			Vector<NamedayCsvLoader.Day> vDay) {
 		this.mContext = context;
 		this.lContacts = lContacts;
 		this.vDay = vDay;
-		children = new String[][] { { "Arnold", "Barry", "Chuck", "David" },
-				{ "Ace", "Bandit", "Cha-Cha", "Deuce" },
-				{ "Fluffy", "Snuggles" }, { "Goldy", "Bubbles" } };
 		lIinflater = mContext.getLayoutInflater();
 
 	}
 
 	public View getGroupView(int groupPosition, boolean isExpanded,
 			View convertView, ViewGroup parent) {
-		v = convertView;
-		if (v == null)
-			v = lIinflater.inflate(R.layout.rowitem, null);
+		View view = convertView;
+		
+		if (view == null)
+			view = lIinflater.inflate(R.layout.rowitem, null);
 
-		TextView tv_date = (TextView) v.findViewById(R.id.tv_date);
-		TextView tv_name = (TextView) v.findViewById(R.id.tv_contactName);
-		ImageView iv_iconCall = (ImageView) v.findViewById(R.id.iv_iconCall);
-		ImageView iv_iconMess = (ImageView) v.findViewById(R.id.iv_iconMess);
+		TextView tv_date = (TextView) view.findViewById(R.id.tv_date);
+		TextView tv_name = (TextView) view.findViewById(R.id.tv_contactName);
 		String dateName = vDay.get(groupPosition).dateName;
 
 		// display number of known contacts for day name in the calendar
@@ -49,24 +43,10 @@ public class CustomAdapter extends BaseExpandableListAdapter {
 			dateName += " (" + vDay.get(groupPosition).contacts.size() + ")";
 		}
 
-//		tv_name.setText(((Contact) lContacts.get(position)).getName());
 		tv_date.setText(vDay.get(groupPosition).date);
 		tv_name.setText(dateName);
 		
-//		iv_iconMess.setImageResource(R.drawable.icon_idroid_sms);
-//		iv_iconCall.setImageResource(R.drawable.icon_idroid_phone);
-//		iv_iconMess.setImageResource(R.drawable.icon_j_sms);
-//		iv_iconCall.setImageResource(R.drawable.icon_j_phone);
-		iv_iconMess.setImageResource(R.drawable.icon_nixus_sms);
-//		iv_iconCall.setImageResource(R.drawable.icon_nixus_phone);
-//		iv_iconMess.setImageResource(R.drawable.icon_ww_sms);
-		iv_iconCall.setImageResource(R.drawable.icon_ww_phone);
-		// click listener - phone icon
-		iv_iconCall.setOnClickListener(new mListener(mContext));
-		// click listener - sms icon
-		iv_iconMess.setOnClickListener(new mListener(mContext));
-		
-		return v;
+		return view;
 	}
 
 	public Object getChild(int groupPosition, int childPosition) {
@@ -80,8 +60,8 @@ public class CustomAdapter extends BaseExpandableListAdapter {
 	public View getChildView(int groupPosition, int childPosition,
 			boolean isLastChild, View convertView, ViewGroup parent) {
 		View view = convertView;
+		
 		if (view == null)
-			// nevim proc vyhazuje vyjimku ?
 			view = lIinflater.inflate(R.layout.rowitem, null);
 		
 		Contact contact = (Contact) getChild(groupPosition, childPosition);
@@ -92,8 +72,8 @@ public class CustomAdapter extends BaseExpandableListAdapter {
 		ImageView iv_iconMess = (ImageView) view.findViewById(R.id.iv_iconMess);
 		
 		tv_name.setText(name);
-		iv_iconCall.setImageResource(R.drawable.phone_icon);
-		iv_iconMess.setImageResource(R.drawable.sms_icon);
+		iv_iconCall.setImageResource(R.drawable.icon_nixus_sms);
+		iv_iconMess.setImageResource(R.drawable.icon_ww_phone);
 		iv_iconCall.setOnClickListener(new mListener(mContext));// listener na
 		// event click
 		// ikony phone
