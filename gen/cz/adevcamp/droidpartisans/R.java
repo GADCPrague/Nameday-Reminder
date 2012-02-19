493ad49796bba39d97ac0a267a60634b25495279
reeloo <reeloo@reeloo.net>
2012-02-19 13:48:23 +0100

    row/subrow has its own xml cleaning up icons

--- gen/cz/adevcamp/droidpartisans/R.java
+++ gen/cz/adevcamp/droidpartisans/R.java
@@ -12,27 +12,21 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int icon_idroid_phone=0x7f020001;
-        public static final int icon_idroid_sms=0x7f020002;
-        public static final int icon_j_phone=0x7f020003;
-        public static final int icon_j_sms=0x7f020004;
-        public static final int icon_nixus_phone=0x7f020005;
-        public static final int icon_nixus_sms=0x7f020006;
-        public static final int icon_ww_phone=0x7f020007;
-        public static final int icon_ww_sms=0x7f020008;
+        public static final int icon_nixus_sms=0x7f020001;
+        public static final int icon_ww_phone=0x7f020002;
+        public static final int icon_ww_phone_orig=0x7f020003;
     }
     public static final class id {
-        public static final int button=0x7f060001;
-        public static final int iv_iconCall=0x7f060005;
+        public static final int button=0x7f060000;
+        public static final int iv_iconCall=0x7f060003;
         public static final int iv_iconMess=0x7f060004;
-        public static final int tvTestName=0x7f060000;
-        public static final int tv_contactName=0x7f060003;
-        public static final int tv_date=0x7f060002;
+        public static final int tv_contactName=0x7f060002;
+        public static final int tv_date=0x7f060001;
     }
     public static final class layout {
-        public static final int child_layout=0x7f030000;
-        public static final int main=0x7f030001;
-        public static final int rowitem=0x7f030002;
+        public static final int main=0x7f030000;
+        public static final int rowitem=0x7f030001;
+        public static final int rowsubitem=0x7f030002;
     }
     public static final class raw {
         public static final int namedays_cz=0x7f040000;
--- src/cz/adevcamp/droidpartisans/CustomAdapter.java
+++ src/cz/adevcamp/droidpartisans/CustomAdapter.java
@@ -30,7 +30,7 @@ public class CustomAdapter extends BaseExpandableListAdapter {
 	public View getGroupView(int groupPosition, boolean isExpanded,
 			View convertView, ViewGroup parent) {
 		View view = convertView;
-		
+
 		if (view == null)
 			view = lIinflater.inflate(R.layout.rowitem, null);
 
@@ -41,16 +41,17 @@ public class CustomAdapter extends BaseExpandableListAdapter {
 		// display number of known contacts for day name in the calendar
 		if (false == vDay.get(groupPosition).contacts.isEmpty()) {
 			dateName += " (" + vDay.get(groupPosition).contacts.size() + ")";
+		} else {
 		}
 
 		tv_date.setText(vDay.get(groupPosition).date);
 		tv_name.setText(dateName);
-		
+
 		return view;
 	}
 
 	public Object getChild(int groupPosition, int childPosition) {
-		return vDay.get(groupPosition).contacts.get(childPosition); //children[0][0];
+		return vDay.get(groupPosition).contacts.get(childPosition); // children[0][0];
 	}
 
 	public long getChildId(int groupPosition, int childPosition) {
@@ -60,17 +61,17 @@ public class CustomAdapter extends BaseExpandableListAdapter {
 	public View getChildView(int groupPosition, int childPosition,
 			boolean isLastChild, View convertView, ViewGroup parent) {
 		View view = convertView;
-		
+
 		if (view == null)
-			view = lIinflater.inflate(R.layout.rowitem, null);
-		
+			view = lIinflater.inflate(R.layout.rowsubitem, null);
+
 		Contact contact = (Contact) getChild(groupPosition, childPosition);
 		String name = contact.getName() + " " + contact.getSurname();
-		
+
 		TextView tv_name = (TextView) view.findViewById(R.id.tv_contactName);
 		ImageView iv_iconCall = (ImageView) view.findViewById(R.id.iv_iconCall);
 		ImageView iv_iconMess = (ImageView) view.findViewById(R.id.iv_iconMess);
-		
+
 		tv_name.setText(name);
 		iv_iconCall.setImageResource(R.drawable.icon_nixus_sms);
 		iv_iconMess.setImageResource(R.drawable.icon_ww_phone);
@@ -85,7 +86,7 @@ public class CustomAdapter extends BaseExpandableListAdapter {
 	}
 
 	public int getChildrenCount(int groupPosition) {
-		return vDay.get(groupPosition).contacts.size(); //children.length;
+		return vDay.get(groupPosition).contacts.size(); // children.length;
 	}
 
 	public Object getGroup(int groupPosition) {
