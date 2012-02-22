295f85e37a7036f434150e4924b00f3a485d6c69
Eugen Krizo <eugen.krizo@gmail.com>
2012-02-22 14:45:09 +0100

    namedays of Slovakia from wikipedia

--- gen/com/droidpartisans/namedayreminder/R.java
+++ gen/com/droidpartisans/namedayreminder/R.java
@@ -36,6 +36,7 @@ public final class R {
     public static final class raw {
         public static final int namedays_cz=0x7f040000;
         public static final int namedays_cz_rev=0x7f040001;
+        public static final int namedays_sk_from_wiki=0x7f040002;
     }
     public static final class string {
         public static final int app_name=0x7f050001;
